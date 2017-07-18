1d2bfc6ec730aa2d8e753b53cf02723ae5f37d6b
Sharan Kasandula <sharankasandula@gmail.com>
2017-07-18 17:26:50 +0200

    A simple to do list app

--- app.js
+++ app.js
@@ -1,67 +1,57 @@
-var app = angular.module('noteApp', []);
+var app = angular.module('noteApp', ['restangular', 'base64']);
 
-app.controller('mainCtrl', ['$scope', '$log', function($scope, l) {
+app.config(function(RestangularProvider,$base64) {
+    // Add Base url of the file where you can get the notes from
+    RestangularProvider.setBaseUrl('https://shdw6b6fe553.int.sap.hana.ondemand.com/hana-070849/');
+});
 
+
+
+app.controller('mainCtrl', ['$scope', '$log', 'Restangular', '$http', 
+     function($scope, l, Restangular, $http) {
+
+    Restangular.setFullResponse(true);
     $scope.note = "";
     $scope.notes = [];
+    $scope.allTodos = [];
+    
 
-    // Add home url and the url of the file where yopu can get the notes from
-
-    $scope.toggleAddNote = function() {
-        $scope.addNote = true;
-        l($scope.addNote);
-    };
-
+    var todoId = 1
     $scope.AddNote = function() {
-        $scope.notes.push($scope.note);
-        console.log($scope.notes);
+
+        // POST request that will post the note to the database
+        // Send todoId and the $scope.note to the backend as a POST request
+        var path = 'demo-app-service.xsodata/DATA';
+        var noteDiary = Restangular.all(path);
+        var newNote = { ID : "1323",
+                        NOTE : $scope.note }
+        noteDiary.post(newNote);
         $scope.note = "";
+
+        setTimeout(function() {
+            $scope.getAllNotes();
+        }, 3000);
     };
 
-    $scope.deleteNote = function(index){
-    	console.log(index);
-    	$scope.notes.splice(index,1);
+    $scope.deleteNote = function(id) {
+        console.log(id);
+
+        var query = 'demo-app-service.xsodata/DATA(' + id + ')';
+        // var query = 'demo-app-service.xsodata/DATA';
+        Restangular.one(query).remove(id);
+
+        setTimeout(function() {
+            $scope.getAllNotes();
+        }, 1000);
     }
 
     var homeUrl = '';
 
-    $scope.getAllNotes=function(){
-        $http({
-              method: 'GET',
-              url: homeUrl+'',
-            }).then(function successCallback(response) {
-            if (response.status == 200){
-                resolve(response.data);
-            } else {
-                reject(response.statusText);
-            }
-        });
-    };
-    // Resolving the promise function and assigning the data to $scope.calConfig.
-    $scope.allNotes = $scope.getAllNotes();
-    $scope.allNotes.then(
-        function (allNotes){
-            $scope.notes = allNotes;
-        },
-        function (error){
-            console.log(error);
-        }
-    );
-
-    // POST
-    $http({
-          method: 'POST',
-          url: homeUrl + '',
-          data: notes,
-          headers: {'Content-Type': 'notes'}
-        }).then(function successCallback(response) {
-        if (response.status == 200){
-            resolve(response.data);
-        } else {
-            reject(response.statusText);
-        }
-
-
-
+    $scope.getAllNotes = function() {
+            Restangular.one('demo-app-service.xsodata/DATA').get().then(function(result){
+                $scope.allTodos = result.data.d.results;
+                // console.log(result.data.d.results)
+            });
+        };
 
 }])
